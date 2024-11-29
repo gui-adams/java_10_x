4e31650bf1806d20ad0fd371bd72286e687536db
gui-adams <guilherme.adams@live.com>
2024-11-28 21:13:54 -0300

    DadosPrimitivos

---
+++ Java/iniciante/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,27 @@
+package Java.iniciante.TiposDeDados;
+   /*
+Classes: Use PascalCase → TipagemDeDados
+Métodos e variáveis: Use camelCase → tipagemDados ou getDados
+
+Declaração
+PSVM : como atalho para public static vold main(String[] args)
+ */
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+        *Dados primitivos - int, double, float, chat, boolean, short.
+        * Objetivo da aula: criar um ninja - Naruto -
+        * No java sempre termine com ponto e virgula para finalizar o trecho de código
+         */
+
+        int idade = 16; // numero (valor maximo 2 147 483 647)
+        double altura = 1.65; // numero flutuante (com casas decimais)
+        char inicial = 'N'; // char precisa passar entre aspas simples
+        boolean vivoouMorto = true; // só vai de true ou false
+        Long saldoBancario = 3147483649L; //Sempre começa com letra maiscula e termina com com L no final.
+        // Numeros acima de 2b (9, 223, 372, 036, 854, 775, 807) // YOUTUBE PERDEU 50 MILHORES DE DOLARES COM O Psy
+
+
+    }
+}

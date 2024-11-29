555eaed405e7d8611cbd3d64a832b97692c2805d
gui-adams <guilherme.adams@live.com>
2024-11-28 21:47:52 -0300

    DadosNãoPrimitivos

---
+++ Java/iniciante/TiposDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,4 @@
+package Java.iniciante.TiposDeDados;
+
+public class DadosNaoPrimitivos {
+}
--- Java/iniciante/TiposDeDados/DadosPrimitivos.java
+++ Java/iniciante/TiposDeDados/DadosPrimitivos.java
@@ -15,13 +15,17 @@ public class DadosPrimitivos {
         * No java sempre termine com ponto e virgula para finalizar o trecho de código
          */
 
-        int idade = 16; // numero (valor maximo 2 147 483 647)
+        int idade = 17; // numero (valor maximo 2 147 483 647)
         double altura = 1.65; // numero flutuante (com casas decimais)
         char inicial = 'N'; // char precisa passar entre aspas simples
         boolean vivoouMorto = true; // só vai de true ou false
         Long saldoBancario = 3147483649L; //Sempre começa com letra maiscula e termina com com L no final.
         // Numeros acima de 2b (9, 223, 372, 036, 854, 775, 807) // YOUTUBE PERDEU 50 MILHORES DE DOLARES COM O Psy
 
+        //print vai ser SOUT
+
+        System.out.println(idade); //Comando para mostrar o dado
+        System.out.println("Saldo bancario é = " + saldoBancario);
 
     }
 }

03afc6d684af53850da0882e1574d33bfcf66004
gui-adams <guilherme.adams@live.com>
2025-09-07 20:47:52 -0300

    DesafioOne

---
+++ src/DesafioOne/DesafioOne.java
@@ -0,0 +1,32 @@
+package DesafioOne;
+
+public class DesafioOne {
+    public static void main(String[] args) {
+        //ninja 1
+        String nomeNinja1 = "Naruto";
+        int idadeNinja1 = 12;
+        String missao1 = "Criar um rasegan nivel fogo";
+        String status1 = "Em andamento";
+        char nivel1 = 'S';
+
+        System.out.println("Ninja: " + nomeNinja1);
+        System.out.println("Idade: " + idadeNinja1);
+        System.out.println("Missão em andamento: " + missao1);
+        System.out.println("Status: " + status1);
+        System.out.println("Nivel: " + nivel1);
+
+        //ninja 2
+        String nomeNinja2 = "Sasuke";
+        int idadeNinja2 = 13;
+        String missao2 = "ensinar chidore";
+        String status2 = "Concluido";
+        char nivel2 = 'A';
+
+        //ninja 3
+        String nomeNinja3 = "Sakura";
+        int idadeNinja3 = 14;
+        String missao3 = "cirurgia ocular";
+        String status3 = "Não iniciado";
+        char nivel3 = 'B';
+    }
+}
